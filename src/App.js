3e5bc62b3f2ddf2baa4cce43b7dbccb42871e983
josip <rastocic.josip@gmail.com>
2020-01-29 21:07:11 +0100

    kita marjanova 2

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom'
-import { Drawer } from '@material/drawer'
 import ShopPage from  './pages/shop/shop.component.jsx'
+import Header from './components/header/header-component.jsx'
 
 function App() {
   return (
     <div>
+    <Header/>
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
