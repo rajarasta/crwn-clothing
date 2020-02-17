503ef65c487cf5a1083afc16b1153719598f60f6
josip <rastocic.josip@gmail.com>
2020-02-17 18:47:10 +0100

    poruka

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from './cart.types';
-import { addItemToCart } from './cart.utils'
+import { addItemToCart, removeItemFromCart } from './cart.utils'
 
 const INITIAL_STATE = {
     hidden: true,
@@ -18,6 +18,25 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state,
                 cartItems: addItemToCart(state.cartItems, action.payload)
+            }
+
+        /*Prekopira cijeli state, ali updateje prop cartItems sa parametrom koji je prebacen preko payloada*/
+        /*Poziva na postjeci cartItems filter, passing cartItem sa i ako ne odgovara cartItem payloadu onda nista
+        ako odgovara onda ga izfiltrira. Filter vraca sve sto je true nazad, ono sto nije true izbacuje*/
+        /*Na kraju treba bindat cart na checkout-item component*/
+        case CartActionTypes.CLEAR_ITEM_FROM_CART:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(
+                    cartItem => cartItem.id !== action.payload.id
+                )
+            }
+
+        case CartActionTypes.REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
+
             }
         default:
             return state;
